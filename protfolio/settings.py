a2567d3f5d25add213742382a0fc7ab5713fdd40
Shubh Rai <shubhlusiferrai@gmail.com>
2024-01-17 22:06:56 +0530

    final try
    Signed-off-by: Shubh Rai <shubhlusiferrai@gmail.com>

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "website",
+    
 ]
 
 MIDDLEWARE = [
@@ -123,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,"protfolio/static")
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'protfolio/static/'),
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'protfolio/static/')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'protfolio/static/'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'protfolio/static/')
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
