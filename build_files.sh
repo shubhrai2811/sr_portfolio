5c53854eda96a21c4b1e70714422d0fda071b3a1
Shubh Rai <shubhlusiferrai@gmail.com>
2024-01-17 21:58:40 +0530

    final try
    Signed-off-by: Shubh Rai <shubhlusiferrai@gmail.com>

--- build_files.sh
+++ build_files.sh
@@ -2,15 +2,19 @@
 
 # Install necessary dependencies
 apt-get update
-apt-get install -y postgresql-server-dev-all build-essential
+apt-get install -y postgresql-server-dev-all build-essential python3 python3-pip git
+
+# Get PostgreSQL version
+PG_VERSION=$(pg_config --version | grep -o -E '[0-9]+\.[0-9]+')
+PG_CONFIG_PATH="/usr/pgsql-${PG_VERSION}/bin/pg_config"
 
 # Clone psycopg2 source code
 git clone https://github.com/psycopg/psycopg2.git
 cd psycopg2
 
 # Build and install psycopg2
-python setup.py build_ext --pg-config /usr/pgsql-<version>/bin/pg_config build
-python setup.py install
+python3 setup.py build_ext --pg-config "${PG_CONFIG_PATH}" build
+python3 setup.py install
 
 # Cleanup
 cd ..
